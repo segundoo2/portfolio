ee16302d8c799ebcdd2cb3a823d5617156a4a8c7
Edilson Segundo <edilsonsilvaa13@gmail.com>
2025-01-06 13:12:20 -0300

    modifica estilização do sobre mim

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { DownloadButtonComponent } from '../download-button/download-button.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DownloadButtonComponent
   ],
   imports: [
     CommonModule
