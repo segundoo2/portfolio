2d393df215a7c085e4c77048af25d91f9cb6a12f
Edilson Segundo <edilsonsilvaa13@gmail.com>
2025-01-06 13:10:10 -0300

    Adiciona componente download-button ao home

---
+++ src/app/download-button/download-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download-button',
+  standalone: false,
+  
+  templateUrl: './download-button.component.html',
+  styleUrl: './download-button.component.scss'
+})
+export class DownloadButtonComponent {
+
+}
